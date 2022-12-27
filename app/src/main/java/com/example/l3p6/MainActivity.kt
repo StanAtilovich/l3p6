a5e4f03cf17b706e2ec154e471ae690c30406cfe
StanAtilovich <lukin.konstantin.cheb@bk.ru>
2022-12-27 15:51:03 +0300

    hurt

--- app/src/main/java/com/example/l3p6/MainActivity.kt
+++ app/src/main/java/com/example/l3p6/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.l3p6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val likeButton = findViewById<ImageButton>(R.id.likes)
+        likeButton.setImageResource(R.drawable.ic_baseline_favorite_24)
+
+
+        val shareButton = findViewById<ImageButton>(R.id.shares)
+        shareButton.setOnClickListener {
+
+        }
     }
 }
